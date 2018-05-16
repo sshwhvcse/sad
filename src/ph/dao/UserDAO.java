0bc6b38badd7a2026031839362ed026f21e38fe8
amazing <25411>
2018-05-16 11:46:03 +0800

    amazing!!

--- src/ph/dao/UserDAO.java
+++ src/ph/dao/UserDAO.java
@@ -1,10 +1,14 @@
 package ph.dao;
 
-import ph.po.User;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+//import ph.po.Speciality;
+import ph.po.User;
 
 public class UserDAO
 {
